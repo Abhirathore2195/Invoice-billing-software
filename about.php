8f42735c9e23828568ea67b2131baba7e8b22bdc
Abhishek Rathore <106559280+Abhirathore2195@users.noreply.github.com>
2022-09-07 09:18:09 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php include('./constant/layout/head.php');?>
+<?php include('./constant/layout/header.php');?>
+
+<?php include('./constant/layout/sidebar.php');?>   
+ 
+        <div class="page-wrapper">
+            
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">Add Brand Management</h3> </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">Add Brand</li>
+                    </ol>
+                </div>
+            </div>
+            
+            
+            <div class="container-fluid">
+                <!--  Author Name: Abhishek. 
+ for any PHP, Codeignitor or Laravel work visit www.Abhishek.com  -->
+                
+                
+                
+             
+<?php include('./constant/layout/footer.php');?>
+<!--  Author Name: Abhishek. 
+ for any PHP, Codeignitor or Laravel work contact me at Abhishek.infospace@gmail.com  -->
+
